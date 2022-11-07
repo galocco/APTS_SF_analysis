302a02c0e55405fa6ba64fa48e71f03e517fe33a
galocco <giacomo.alocco@edu.unito.it>
2022-11-07 17:06:32 +0100

    fix fhr

--- eff_fhr_vs_thr.py
+++ eff_fhr_vs_thr.py
@@ -2,6 +2,13 @@ import ROOT
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-a', '--adc', help='Plot in ADCu', action='store_true')
+args = parser.parse_args()
+
+ADCu = args.adc
 
 hundredElectronToADCu = {
               "AF15_W13_1.2V":   113.5602885,
@@ -19,27 +26,25 @@ hundredElectronToADCu = {
                "AF15P_W22B11":   80.27620951,
                "AF15P_W22B15":   85.8128149
             }
+            
 
 #list of the analysis results path
-path="/Users/alicemulliri/CORRY/LastCorry/its-corryvreckan-tools/output/SPSOctober22/B1/AF15P_W22B2/"
-file_path_list = [path+"alignment_000801_DUT_cut60_3.root",
-                  path+"alignment_000802_DUT_cut60_3.root",
-                  path+"alignment_000803_DUT_cut60_3.root",
-                  path+"alignment_000804_DUT_cut60_3.root",
-                  path+"alignment_000805_DUT_cut60_3.root"
+file_path_list = [
+                  "SPSRes/alignment_416153058221015153104_DUT_cut60_3.root",
+                  "SPSRes/alignment_422131231221018134906_DUT_cut60_3.root",
+                  "SPSRes/alignment_000801_DUT_cut60_3.root"
          ]
 
-list_of_files = [path+"alignment_000801_DUT_cut60_3.root",
-                  path+"alignment_000802_DUT_cut60_3.root",
-                  path+"alignment_000803_DUT_cut60_3.root",
-                  path+"alignment_000804_DUT_cut60_3.root",
-                  path+"alignment_000805_DUT_cut60_3.root"
+list_of_files = [
+                  "SPSRes/alignment_416153058221015153104_DUT_FHR.root",
+                  "SPSRes/alignment_422131231221018134906_DUT_FHR.root",
+                  "SPSRes/alignment_000801_DUT_FHR.root"
          ]
 #list of the chips
 #list of the chips
-chip_list = ["AF15P_W22_0V","AF15P_W22_1.2V","AF15P_W22_2.4V","AF15P_W22_3.6V","AF15P_W22_4.8V"]
+chip_list = ["AF15P_W22_0V","AF15P_W22_1.2V","AF15P_W22_1.2V"]
 #list of the labels for the plot
-label_list = ["0V","1.2V","2.4V","3.6V","4.8V"]
+label_list = ["25P","10B","15P"]
 
 color_list = ['b', 'black', 'r', 'g', 'orange']
 
@@ -53,7 +58,7 @@ ax2 = ax1.twinx()
 ax1.errorbar([],[],([],[]),label="Efficiency",marker='s',elinewidth=1.3,capsize=1.5,color='dimgrey')
 ax1.errorbar([],[],([],[]),label="Fake Hit Rate",marker='s',markerfacecolor='none',linestyle='dashed',elinewidth=1.3,capsize=1.5,color='dimgrey')
 
-list_of_thr = [60,80,100,120,150,170,200,225,250]
+list_of_thr = [60,80,100,120,150,170,200,225,250,300]
 
 
 for file_path,label,chip,color in zip(file_path_list,label_list,chip_list,color_list):
@@ -87,7 +92,10 @@ for file_path,label,chip,color in zip(file_path_list,label_list,chip_list,color_
       eff_list.append(100*eff.GetEfficiency(1))
       err_eff_up.append(100*eff.GetEfficiencyErrorUp(1))
       err_eff_low.append(100*eff.GetEfficiencyErrorLow(1))
-      charge.append(100*thr/hundredElectronToADCu[chip])
+      if ADCu:
+        charge.append(100*thr/hundredElectronToADCu[chip])
+      else:
+        charge.append(thr)
       del passed
       del eff
 
@@ -103,25 +111,18 @@ for file,label,color,chip in zip(list_of_files, label_list, color_list,chip_list
     else:
         hist = tfile.Get("EventLoaderEUDAQ2/APTS_3/hPixelRawValues")#To be changed for June data
 
-    thrs_list = []
-    bins_list = []
     charge =  []
-    for thr in list_of_thr:
-        for k in range(1, hist.GetNbinsX()):
-            if hist.GetBinLowEdge(k) > thr:
-                bins_list.append(k)
-                thrs_list.append(hist.GetBinLowEdge(k))
-                break
-        charge.append(100*thr/hundredElectronToADCu[chip])
-
-    #    charge.append(100*thr/hundredElectronToADCu[chip])
-    n_events = (hist.GetEntries()-hist.GetBinContent(0))/n_pixels
     fake_hit_rate_list = []
+    n_events = (hist.GetEntries()-hist.GetBinContent(0))/n_pixels
 
-    for bin_min in bins_list:
+    for thr in list_of_thr:
+        bin_min = hist.GetXaxis().FindBin(thr)
         clusters = hist.Integral(bin_min, hist.GetNbinsX()+1)
         fake_hit_rate_list.append(clusters/n_pixels/n_events)
-
+        if ADCu:
+            charge.append(100*thr/hundredElectronToADCu[chip])
+        else:
+            charge.append(thr)
     ax2.errorbar(charge, fake_hit_rate_list, label=label, marker="s", linestyle='dashed',markerfacecolor='none',color=color)
 
 
@@ -129,7 +130,10 @@ ax2.set_ylabel("P(pixel above Threshold)")
 #ax2.set_yscale('log')
 
 ax1.set_ylabel('Efficiency (%)')
-ax1.set_xlabel('Threshold (electrons)')
+if ADCu:
+    ax1.set_xlabel('Threshold (ADCu)')
+else:
+    ax1.set_xlabel('Threshold (electrons)')
 ax1.grid(axis='both')
 ax1.set_ylim(69,101)
 ax2.set_ylim(0,1)
@@ -137,8 +141,6 @@ ax2.minorticks_off()
 ax1.axhline(99,linestyle='dashed',color='grey')
 ax1.text(ax1.get_xlim()[0]-0.014*(ax1.get_xlim()[1]-ax1.get_xlim()[0]),99,"99",fontsize=7,ha='right', va='center')
 h1, l1 = ax1.get_legend_handles_labels()
-#h2, l2 = ax2.get_legend_handles_labels()
-#ax1.legend(h1[:1]+h2+h1[1:],l1[:1]+l2+l1[1:],loc='lower center',bbox_to_anchor=(1.22, -0.01),prop={"size":9})
 ax1.legend(h1[:1]+h1[1:],l1[:1]+l1[1:],loc='lower center',bbox_to_anchor=(1.22, -0.01),prop={"size":9})
 
 x = 0.75
@@ -146,7 +148,7 @@ y = 0.9
 
 ax1.text(
     x,y,
-    '$\\bf{ALICE\ ITS3}$ beam test $\\it{preliminary}$',
+    '$\\bf{ALICE\ ITS3}$ beam test WORK IN PROGRESS',
     fontsize=12,
     ha='center', va='top',
     transform=ax1.transAxes
@@ -178,5 +180,8 @@ ax1.text(1.1,1.0,
     transform=ax1.transAxes
     )
 
-fig.savefig('vothVsThreshold.png', dpi=600)
-plt.show()
+if ADCu:
+    fig.savefig('eff_fhr_VsThresholdInADCu.png', dpi=600)
+else:
+    fig.savefig('eff_fhr_VsThreshold.png', dpi=600)
+#plt.show()
